fc238cd674456ec09a0fd666eb47af1c8605a2a2
Silenthinker <sparklethinker@gmail.com>
2017-06-30 15:23:36 +0800

    uptodate

--- cnn_model.py
+++ cnn_model.py
@@ -20,8 +20,8 @@ def CNN(args, inputs, is_training=True):
         # padding='SAME', activation_fn=nn.relu,
         # weights_initializer = initializers.xavier_initializer(),
         # biases_initializer = init_ops.zeros_initializer,
-        net = slim.conv2d(x, 16, [5, 5], scope='conv1')
-        net = slim.max_pool2d(net, [2, 2], scope='pool1')
+        net = slim.conv2d(x, 16, [10, 10], stride=4, scope='conv1') # 246 -> 60
+        net = slim.max_pool2d(net, [5, 5], scope='pool1')
         net = slim.conv2d(net, 32, [5, 5], scope='conv2')
         net = slim.max_pool2d(net, [2, 2], scope='pool2')
         net = slim.conv2d(net, 64, [5, 5], scope='conv3')
@@ -40,3 +40,26 @@ def CNN(args, inputs, is_training=True):
         else: # regression
             outputs = slim.fully_connected(net, 1, activation_fn=tf.nn.relu, normalizer_fn=None, scope="fco")
     return outputs
+'''
+def vgg16(inputs):
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                          activation_fn=tf.nn.relu,
+                          weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
+                          weights_regularizer=slim.l2_regularizer(0.0005)):
+        net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], scope='pool1')
+        net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], scope='pool2')
+        net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+        net = slim.max_pool2d(net, [2, 2], scope='pool3')
+        net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+        net = slim.max_pool2d(net, [2, 2], scope='pool4')
+        net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+        net = slim.max_pool2d(net, [2, 2], scope='pool5')
+        net = slim.fully_connected(net, 4096, scope='fc6')
+        net = slim.dropout(net, 0.5, scope='dropout6')
+        net = slim.fully_connected(net, 4096, scope='fc7')
+        net = slim.dropout(net, 0.5, scope='dropout7')
+        net = slim.fully_connected(net, 1, activation_fn=tf.nn.relu, scope='fc8')
+    return net
+'''
